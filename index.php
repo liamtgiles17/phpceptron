bc2c76efc39819b3cb8cd04278a3c0561f0dae35
liamtgiles17 <liamthomasgiles1702@gmail.com>
2024-10-28 17:39:04 +0000

    added heat map for perceptron weight

--- index.php
+++ index.php
@@ -14,11 +14,27 @@
         <h1>PHP Perceptron Demo</h1>
         <p>Click canvas to re-train and test model.</p>
         <canvas id="weights" width="540" height="540"></canvas>
+        <div><br/></div>
+        <canvas id="scale" width="540" height="50"></canvas>
         <script>
             $(document).ready(() => {
                 const canvas = document.getElementById("weights");
                 const ctx = canvas.getContext("2d");
                 ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+                const scaleCanvas = document.getElementById("scale");
+                const scaleCtx = scaleCanvas.getContext("2d");
+                const grad = scaleCtx.createLinearGradient(0, 0, 540, 0);
+                grad.addColorStop(0, `rgb(${255-((60*2)+127)}, ${(60*2)+127}, ${255-((60*2)+127)})`);
+                grad.addColorStop(1, `rgb(${255-((-60*2)+127)}, ${(-60*2)+127}, ${255-((-60*2)+127)})`);
+                scaleCtx.fillStyle = grad;
+                scaleCtx.fillRect(0, 0, 540, 30);
+                scaleCtx.fillStyle = "rgb(0, 0, 0)";
+                scaleCtx.font = "12px Serif";
+                scaleCtx.fillText("60", 0, 50);
+                scaleCtx.fillText("Weight", 250, 45);
+                scaleCtx.fillText("-60", 522, 50);
+
                 var WIDTH = 30;
                 var HEIGHT = 30;
                 var cellWidth = Math.floor(ctx.canvas.width/WIDTH);
@@ -31,11 +47,11 @@
                     for (let i = 0; i < WIDTH; i++) {
                         for (let j = 0; j < HEIGHT; j++) {
                             let weight = weights[j][i];
-                            weight += 127;
                             weight *= 2;
+                            weight += 127;
                             if (weight <= 0) weight = 0;
                             else if (weight >= 255) weight = 255;
-                            ctx.fillStyle = `rgb(127, ${weight}, ${weight})`;
+                            ctx.fillStyle = `rgb(${255-weight}, ${weight}, ${255-weight})`;
                             ctx.fillRect(i*cellWidth, j*cellHeight, cellWidth, cellHeight); 
                         }
                     }
